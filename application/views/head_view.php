5c36d650ef3e23b283598cd24e3816b7bc9ba175
Ryan Partington <pinescore@outlook.com>
2020-01-22 14:25:47 +0000

    updated refresh time

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -5,7 +5,7 @@
     <title><?php echo $title; ?></title>
     <meta name="description" content="<?php echo $description; ?>">
     <meta name="keywords" content="<?php echo $keywords; ?>">
-    <meta http-equiv="refresh" content="5" >
+    <meta http-equiv="refresh" content="120" >
     <link rel="stylesheet" type="text/css" href="112/Styles.css">
 </head>
 <body>
